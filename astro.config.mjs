73d2efe09a4ced3cf8150401c67db35c65dfe914
John Potter <john.m.potter@protonmail.com>
2025-06-23 01:08:57 -0400

    Added missing PageLayout.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import rss from '@astrojs/rss';
-import ts from '@astrojs/typescript';
-
 export default defineConfig({
   site: 'http://localhost:4321',
-  integrations: [tailwind(), mdx(), sitemap(), rss(), ts()],
+  integrations: [tailwind()],
 });
